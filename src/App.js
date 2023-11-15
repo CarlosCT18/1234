abab772bd51de62b2fd3dddb93bf19a021e93acf
AndresCT1 <carlos.calvo@ucsm.edu.pe>
2023-11-15 11:55:52 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -5,18 +5,15 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Instituto Honorio Delgado
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>
+          Carlos Calvo Ticona
+        </p>
+        <p>
+          Turno Noche
+        </p>
       </header>
     </div>
   );
